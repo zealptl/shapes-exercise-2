3f7056ccf8f56a74cdfd6cc4efd080622198c8cc
zealptl <zealpatel06@gmail.com>
2020-03-25 23:46:54 -0400

    refactored variables

--- src/sample/MyOval.java
+++ src/sample/MyOval.java
@@ -1,59 +1,58 @@
 package sample;
 
-import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 
 public class MyOval extends MyShape {
 
     //Variables
-    private double xLength;
-    private double yLength;
+    private double width;
+    private double height;
 
     //Constructors
     public MyOval() {
         super(0,0);
-        this.xLength = 0;
-        this.yLength = 0;
+        this.width = 0;
+        this.height = 0;
     }
-    public MyOval(double x, double y, double xLength, double yLength) {
-        super(x - xLength/2, y - yLength/2);
-        this.xLength = xLength;
-        this.yLength = yLength;
+    public MyOval(double x, double y, double width, double height) {
+        super(x - width/2, y - height/2);
+        this.width = width;
+        this.height = height;
     }
 
     //Getters
-    public double getxLength() { return xLength; }
-    public void setyLength(double yLength) { this.yLength = yLength; }
+    public double getWidth() { return width; }
+    public void setHeight(double height) { this.height = height; }
 
     //Setters
-    public void setxLength(double xLength) { this.xLength = xLength; }
-    public double getyLength() {
-        return yLength;
+    public void setWidth(double width) { this.width = width; }
+    public double getHeight() {
+        return height;
     }
 
     //MyOval specific methods
     public double getPerimeter() {
-        double a = this.xLength/2;
-        double b = this.yLength/2;
+        double a = this.width /2;
+        double b = this.height /2;
         return Math.PI * ((3*(a+b)) - Math.sqrt((3*a + b)*(a + 3*b)));
     }
     public double getArea() {
-        return Math.PI * (this.xLength/2) * this.yLength/2;
+        return Math.PI * (this.width /2) * this.height /2;
     }
 
     //Overridden methods from MyShape
     public String toString() {
-        return this.getClass().getName() + "\nX Axis length: " + this.xLength + "\nY Axis length: " + this.yLength
+        return this.getClass().getName() + "\nX Axis length: " + this.width + "\nY Axis length: " + this.height
                 + "\nPerimeter: " + this.getPerimeter() + "\nArea: " + this.getArea();
     }
     public void draw(GraphicsContext gc) {
         gc.setFill(this.color.getColor());
-        gc.fillOval(this.x, this.y, this.xLength, this.yLength);
+        gc.fillOval(this.x, this.y, this.width, this.height);
     }
-    
+
     //Overridden methods from MyShapePosition
     public MyRectangle getBoundingBox() {
-        return new MyRectangle(this.x + this.xLength/2, this.y + this.yLength/2, this.xLength, this.yLength);
+        return new MyRectangle(this.x + this.width /2, this.y + this.height /2, this.width, this.height);
     }
     public boolean doOverlap(MyShape shape2) {
         return doMyRectangleOverlap(this.getBoundingBox(), shape2.getBoundingBox());
